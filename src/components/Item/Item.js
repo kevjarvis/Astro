2c3f62672fb0a05b3f598b2a363abf5e65064398
Kevin Castillo <kcastilloc1@uft.edu>
2022-04-18 11:49:43 -0400

    Evento click en imágen de producto

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,14 +1,19 @@
 import { ChevronRight } from "react-bootstrap-icons";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Rating from "../Rating/Rating";
 import ProductPrice from "../ProductPrice/ProductPrice";
 import './Item.css';
 
 const Item = ( { item } ) => {
+  const navigate = useNavigate();
+
+  const handleClick = (id) => {
+    navigate(`/detail/${id}`)
+  }
 
   return (
     <div className={ item.stock ? 'card-product' : 'card-product noStock'}>
-      <img src={item.pictureUrl} className={'product-img'} alt={item.title} />
+      <img onClick={ () => handleClick(item.id) } src={item.pictureUrl} className={'product-img'} alt={item.title} />
       <span className={'product-name'}>{item.title}</span>
       <Rating rating={item.rating} />
       <ProductPrice prices={item.prices}/>
